a9827c5fd769ed58fbf79b3976718ea59fe1c492
LAURENT MATHIS p1406269 <mathis.laurent@etu.univ-lyon1.fr>
2017-05-04 16:07:32 +0200

    update layout

--- app/src/main/java/com/example/iem/arduinocontrol/MainActivity.java
+++ app/src/main/java/com/example/iem/arduinocontrol/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.iem.arduinocontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_noconnect;
+    Button butchange;
+    LinearLayout bg;
+    boolean basemode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_noconnect = (TextView) findViewById(R.id.tv_noconnect);
+        butchange = (Button) findViewById(R.id.butchangemod);
+        bg = (LinearLayout) findViewById(R.id.ll_bg);
+        basemode = false;
+        butchange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                basemode = true;
+                //bg.setBackgroundColor(R.color.colorPrimary);
+            }
+        });
     }
 }
